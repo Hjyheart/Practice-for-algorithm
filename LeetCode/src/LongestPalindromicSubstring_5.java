ce6d8412b7163c8535a81654af8eb09695c28e32
洪嘉勇 <tjhjyisland@gmail.com>
2017-11-30 01:10:38 +0800

    have problem in line 98

--- LeetCode/src/LongestPalindromicSubstring_5.java
+++ LeetCode/src/LongestPalindromicSubstring_5.java
@@ -5,42 +5,100 @@ package src;
  */
 public class LongestPalindromicSubstring_5 {
 
-    public static boolean vertify(String s) {
-        for (int i = 0; i < s.length() / 2; i++) {
-            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
-                return false;
+
+    public static String dpSolution(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        boolean dp[][] = new boolean[s.length()][s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int k = 0; k < s.length(); k++) {
+                if (i == k || k == i - 1) {
+                    dp[i][k] = true;
+                } else {
+                    dp[i][k] = false;
+                }
             }
         }
-        return true;
-    }
 
-    public static String longestPalindrome(String s) {
-//        if (s.length() <= 1) {
-//            return s;
-//        }
+        String maxString = s.substring(0, 1);
+
         int start = 0, end = 0;
-        String maxString = "";
-        while (end <= s.length()) {
-            for (int i = end; i >= 0; i--) {
-                if (2 * end - i >= s.length()) {
-                    if (s.substring(i + 1, 2 * end - i).length() > maxString.length()) {
-                        maxString = s.substring(i + 1, 2 * end - i);
+
+        while (end < s.length()) {
+            for (start = 0; start < end; start++) {
+                if (dp[start][end]) {
+                    if (end - start + 1 > maxString.length()) {
+                        maxString = s.substring(start, end + 1);
                     }
-                    break;
-                }
-                if (s.charAt(i) != s.charAt(2 * end - i)) {
-                    if (s.substring(i + 1, 2 * end - i).length() > maxString.length()) {
-                        maxString = s.substring(i + 1, 2 * end - i);
+                } else {
+                    if (s.charAt(start) == s.charAt(end) && dp[start + 1][end - 1]) {
+                        dp[start][end] = true;
+                        if (end - start + 1 > maxString.length()) {
+                            maxString = s.substring(start, end + 1);
+                        }
                     }
-                    break;
                 }
             }
             end++;
         }
+
         return maxString;
     }
 
+
+    // Manacher算法
+    public static String pre(String s) {
+        char[] res = new char[2 * s.length() + 1];
+        res[0] = '#';
+        int k = 1;
+        for (int i = 0; i < s.length(); i++, k++) {
+            res[k] = s.charAt(i);
+            k++;
+            res[k] = '#';
+        }
+        return String.valueOf(res);
+    }
+
+
+
+    public static String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        String tmp = pre(s);
+
+        int mx = 0, id = 0;
+        int[] p = new int[tmp.length()];
+
+        for (int i = 1; i < tmp.length() - 1; i++) {
+            p[i] = mx > i ? Math.min(p[2 * id - i], mx - i) : 1;
+
+            while(i + p[i] < tmp.length() && i - p[i] >= 0 && tmp.charAt(i + p[i]) == tmp.charAt(i - p[i])) {
+                p[i]++;
+            }
+
+            if(i + p[i] > mx) {
+                mx = i + p[i];
+                id = i;
+            }
+        }
+
+        int maxLen = 0, index = 0;
+        for(int i = 1; i < tmp.length() - 1; i++) {
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                index = i;
+            }
+        }
+
+        return s.substring((index - maxLen)/2 + 1, (index - maxLen)/2 + maxLen);
+    }
+
     public static void main(String[] args) {
-        System.out.println(longestPalindrome("babad"));
+        System.out.println(longestPalindrome("bb"));
     }
 }
