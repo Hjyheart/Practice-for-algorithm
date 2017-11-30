57646628def394b6c8ba5d2ca8e5e62e7d77d71a
洪嘉勇 <tjhjyisland@gmail.com>
2017-12-01 00:53:52 +0800

    finish 303

--- LeetCode/src/RangeSumQuery_303.java
+++ LeetCode/src/RangeSumQuery_303.java
@@ -2,32 +2,32 @@ package src;
 
 /**
  * Created by I332329 on 11/30/2017.
+ * Tag: DP
+ * Solution:
+ * 没啥好说的
  */
 public class RangeSumQuery_303 {
 
     static class NumArray {
         int[] nums;
-        int[][] dp;
+        int[] dp;
         public NumArray(int[] nums) {
             this.nums = nums;
-            this.dp = new int[nums.length][nums.length];
+            this.dp = new int[nums.length];
+
+            dp[0] = nums[0];
+
+            for (int i = 1; i < nums.length; i++) {
+                dp[i] = dp[i - 1] + nums[i];
+            }
+
         }
 
         public int sumRange(int i, int j) {
-
-            if (dp[i][j] != 0) {
-                return dp[i][j];
-            } else {
-                for (int k = i; k <= j; k++) {
-                    if (k == i) {
-                        dp[i][k] = nums[i];
-                        continue;
-                    }
-                    dp[i][k] = dp[i][k - 1] + nums[k];
-                }
-                return dp[i][j];
+            if (i == 0) {
+                return dp[j];
             }
-
+            return dp[j] - dp[i - 1];
         }
     }
 
