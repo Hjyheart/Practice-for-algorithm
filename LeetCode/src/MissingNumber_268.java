2e8ff850de029adcd750577724313752591b773e
Hjyheart <764796124@qq.com>
2017-12-01 21:10:55 +0800

    finish 53 70 268 I saw a wonderful solution for 268

---
+++ LeetCode/src/MissingNumber_268.java
@@ -0,0 +1,30 @@
+package src;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Created by I332329 on 12/1/2017.
+ * Tag: Others
+ */
+public class MissingNumber_268 {
+
+    public static int missingNumber(int[] nums) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], true);
+        }
+
+        for (int i = 0; i < nums.length + 1; i++) {
+            if (!map.containsKey(i)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(missingNumber(new int[] {3, 0, 1}));
+    }
+}
