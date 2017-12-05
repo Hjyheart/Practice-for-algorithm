0fe9abd9f58ae8fad846ad7749330330c74406f9
Hjyheart <764796124@qq.com>
2017-12-05 18:29:30 +0800

    ...

---
+++ LeetCode/src/DeleteAndEarn_740.java
@@ -0,0 +1,16 @@
+package src;
+
+/**
+ * Created by I332329 on 12/4/2017.
+ */
+public class DeleteAndEarn_740 {
+
+    public static int deleteAndEarn(int[] nums) {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(deleteAndEarn(new int[] {3, 4, 2}));
+    }
+
+}
