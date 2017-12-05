fa4fe43eb2c071cb4dc7eb0995254193faa6c735
洪嘉勇 <tjhjyisland@gmail.com>
2017-12-05 18:28:47 +0800

    finish 739 and 64

---
+++ LeetCode/src/CherryPick_741.java
@@ -0,0 +1,17 @@
+package src;
+
+/**
+ * Created by hongjiayong on 2017/12/5.
+ */
+public class CherryPick_741 {
+
+    public static int cherryPickup(int[][] grid) {
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(cherryPickup(new int[][]{{0,1,-1},{1,0,-1},{1,1,1}}));
+    }
+
+}
