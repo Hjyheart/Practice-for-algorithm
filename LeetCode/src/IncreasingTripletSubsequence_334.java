e4972d583128bbe076be015f0416dc7805554e05
洪嘉勇 <tjhjyisland@gmail.com>
2017-12-15 09:52:56 +0800

    finish 17 230 334 three easy problems

---
+++ LeetCode/src/IncreasingTripletSubsequence_334.java
@@ -0,0 +1,32 @@
+package src;
+
+/**
+ * Created by hongjiayong on 2017/12/15.
+ */
+public class IncreasingTripletSubsequence_334 {
+
+    public static boolean increasingTriplet(int[] nums) {
+        if (nums.length < 3) {
+            return false;
+        }
+        int min = Integer.MAX_VALUE;
+        int secMin = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= min) {
+                min = nums[i];
+            } else if (nums[i] <= secMin) {
+                secMin = nums[i];
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(increasingTriplet(new int[]{5,1,5,5,2,5,4}));
+        System.out.println(increasingTriplet(new int[]{2,1,5,0,3}));
+    }
+}
