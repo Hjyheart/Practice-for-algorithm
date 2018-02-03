a15e82fab8f6876b188d4a6ec0375a332ed25595
洪嘉勇 <tjhjyisland@gmail.com>
2018-02-04 00:45:44 +0800

    finish 95

---
+++ LeetCode/src/LeetCode95_UniqueBinarySearchTreesII.java
@@ -0,0 +1,51 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hongjiayong on 03/02/2018.
+ */
+public class LeetCode95_UniqueBinarySearchTreesII {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public static ArrayList<TreeNode> dfs(int start, int end) {
+        ArrayList<TreeNode> res = new ArrayList<>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+        for (int i = start; i <= end; i++) {
+            ArrayList<TreeNode> lefts = dfs(start, i - 1);
+            ArrayList<TreeNode> rights = dfs(i + 1, end);
+            for (int k = 0; k < lefts.size(); k++) {
+                for (int j = 0; j < rights.size(); j++) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = lefts.get(k);
+                    root.right = rights.get(j);
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static List<TreeNode> generateTrees(int n) {
+        ArrayList<TreeNode> res = new ArrayList<>();
+        if (n == 0) {
+            return res;
+        }
+        res = dfs(1, n);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        generateTrees(3);
+    }
+}
