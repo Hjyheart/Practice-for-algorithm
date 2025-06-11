084d5341fc342e0592113773b7972e6920e456b4
Hjyheart <jhongag@connect.ust.hk>
2025-06-11 20:58:33 +0800

    440 - 2025-06-11 - 2

--- LeetCode/LeetCode440-KThSmallestInLexicographicalOrder.py
+++ LeetCode/LeetCode440-KThSmallestInLexicographicalOrder.py
@@ -1,27 +1,33 @@
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
-        def count_prefix(prefix, n):
-            curr = prefix
-            next_ = prefix + 1
-            count = 0
-            while curr <= n:
-                count += min(n + 1, next_) - curr
-                curr *= 10
-                next_ *= 10
-            return count
+        def countTree(num, n):
+            cnt = 0
+            next = num + 1
+            while num <= n:
+                cnt += min(n + 1, next) - num
+                num *= 10
+                next *= 10
 
-        curr = 1
-        k -= 1  # 从1开始算，跳过第一个
-        while k > 0:
-            count = count_prefix(curr, n)
-            if k >= count:
-                k -= count
-                curr += 1
+            return cnt
+
+        step = 1
+        num = 1
+        while step < k:
+            cnt = countTree(num, n)
+            if step + cnt > k:
+                step += 1
+                num *= 10
+                if step == k:
+                    break
             else:
-                k -= 1
-                curr *= 10
-        return curr
+                step += cnt
+                num += 1
+                if step == k:
+                    break
 
+        return num
+
+# [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]
 # [1, 10, 100, 11, 12, 13, 14, 15, 16, 17, 18, 19, 2, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 3, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 4, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 5, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 6, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 7, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 8, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 9, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
 
 if __name__ == '__main__':
